2dd9cb6f1b93efcc8d540f0b2efe7a29b4365f30
Anwar Sagor <sagor.onnet@gmail.com>
2022-07-18 17:42:31 +0600

    NotFound

--- src/Pages/SharedPage/NotFound.js
+++ src/Pages/SharedPage/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './SharedPage.css';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className='not-found'>
+            <h2>The page you are searching not found</h2>
+            <h3>404</h3>
         </div>
     );
 };
