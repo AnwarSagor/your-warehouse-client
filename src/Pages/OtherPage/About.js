b43d9acd278f3a9ae6378f7246b90efc3b45ee99
Anwar Sagor <sagor.onnet@gmail.com>
2022-07-21 11:08:03 +0600

    About

--- src/Pages/OtherPage/About.js
+++ src/Pages/OtherPage/About.js
@@ -1,9 +1,14 @@
+import './OtherPage.css'
 import React from 'react';
 
 const About = () => {
     return (
-        <div className='container'>
-            About
+        <div id='about-container' className='container'>
+            <h4><strong>Who We Are</strong></h4>
+            <p> We are a global software company enabling Omnichannel retailing.
+                <br />
+                We help brands and retailers to easily scale, reach and delight customers across channels globally.</p>
+
         </div>
     );
 };
